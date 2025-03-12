ad6829e00ef0ef87313157a2f0f8ce5f598a8825
gooloog <ssk022626@gmail.com>
2025-03-12 12:42:58 +0900

    Add files via upload

---
+++ 2025 03 12 박진영.sql
@@ -0,0 +1,11 @@
+CREATE TABLE address(
+    address1 VARCHAR(255),
+    address2 VARCHAR(255),
+    address3 VARCHAR(255),
+    postal_code VARCHAR(255),
+    client_ID VARCHAR(255) 
+);  
+
+insert into address VALUES('안양시','동안구','평촌대로','진영대로','종훈대로');
+
+drop table address;
